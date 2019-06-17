3a1d2e4f537bdf1ae10378026719bb342efb387f
artkra <artkrasnyy@gmail.com>
2019-06-17 19:15:52 +0300

    Few lines in test case.

--- lserver/server_test.go
+++ lserver/server_test.go
@@ -5,12 +5,34 @@ import (
 	"testing"
 )
 
+type Result struct {
+	advance int
+	data    []byte
+	err     error
+}
+
+type ArgsSent struct {
+	payload []byte
+	eof     bool
+}
+
+var argsSent = []ArgsSent{
+	{[]byte("+++idspispopd_____ABCAAAAAAAAAAAAAAZ789102jsAUoek99"), false},
+}
+
+var expected = []Result{
+	{1, []byte("ABCAAAAAAAAAAAAAAZ789102jsAUoek99"), error(nil)},
+}
+
 func TestLSplit(t *testing.T) {
 	// add tests
-	r0, r1, r2 := lSplit([]byte("+++idspispopd_____ABCAAAAAAAAAAAAAAZ789102jsAUoek99"), false)
-	exp0, exp1, exp2 := 1, []byte("ABCAAAAAAAAAAAAAAZ789102jsAUoek99"), error(nil)
+	for i, args := range argsSent {
+		r0, r1, r2 := lSplit(args.payload, args.eof)
 
-	if r0 != exp0 || !bytes.Equal(r1, exp1) || r2 != exp2 {
-		t.Error(r0, string(r1), r2, " != ", exp0, string(exp1), exp2)
+		exp0, exp1, exp2 := expected[i].advance, expected[i].data, expected[i].err
+
+		if r0 != exp0 || !bytes.Equal(r1, exp1) || r2 != exp2 {
+			t.Error(r0, string(r1), r2, " != ", exp0, string(exp1), exp2)
+		}
 	}
 }
